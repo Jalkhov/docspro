c6c58d1762dd3b6be049ae95e00d9465445e3a4d
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2022-02-19 17:40:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,17 +14,20 @@ def download_docs():
 		lng = lang[0]
 		dlc = lang[1]
 
-		print("> Downloading: ", lng.upper())
 		Octo.dowload_folder(target.format(lang=lng, dial=dlc), folder)
 
-def generate_jsons():
-	pass
+def generate_jsons(lang, cov):
+	json_string = json.dumps({'cov':cov})
+	with open(f'for_deploy/{lang}_cov.json', 'w') as outfile:
+	    outfile.write(json_string)
 
 def main(args):
 	if len(args) > 1 and args[1] == "--download":
 		download_docs()
-	elif len(args) > 1 and args[1] == "--jsons":
-		print(args)
+	elif:
+		lang = args[1].split('--')[1]
+		cov = args[2]
+		generate_jsons(lang, cov)
 
 if __name__ == '__main__':
 	main(sys.argv)
