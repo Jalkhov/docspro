04de808984dc6bc3b5d694f0968e34a1a5e73147
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2022-02-19 18:08:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from octodir import Octodir
 import os, sys
 import json
+import requests
 
 repos = [['es', 'es'], ['fr', 'fr'], ['zh', 'zh_CN'], ['fa', 'fa']] # temporal, walk org repos
 target = 'https://github.com/flaskcwg/flask-docs-{lang}/tree/main/docs/locales/{dial}'
 folder = 'for_deploy/docs' # Current working directory
+badge_url = 'https://shields.io/badge/translated-{cov}%25-green'
 
 def download_docs():
 	Octo = Octodir()
-
+	os.makedirs('data', exist_ok=True) # Create folder for jsons and badges
 	os.makedirs(folder, exist_ok=True)
 	for lang in repos:
 		lng = lang[0]
@@ -17,9 +19,16 @@ def download_docs():
 		Octo.dowload_folder(target.format(lang=lng, dial=dlc), folder)
 
 def generate_jsons(lang, cov):
+	cov = "%.2f" % (float(cov))
 	json_string = json.dumps({'cov':cov})
-	with open(f'for_deploy/{lang}_cov.json', 'w') as outfile:
+	with open(f'data/{lang}_cov.json', 'w') as outfile:
 	    outfile.write(json_string)
+
+def generate_badge(lang, cov):
+	svg_data = requests.get(badge_url.format(cov)).text
+    with open(f'data/{lang}_progress.svg', 'w') as file:
+        file.write(svg_data)
+
 
 def main(args):
 	if len(args) > 1 and args[1] == "--download":
@@ -28,6 +37,7 @@ def main(args):
 		lang = args[1].split('--')[1]
 		cov = args[2]
 		generate_jsons(lang, cov)
+		generate_badge(lang, cov)
 
 if __name__ == '__main__':
 	main(sys.argv)
