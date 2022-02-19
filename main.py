b703f175a708f7b2dca750ca85a616d86dbf78e6
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2022-02-19 10:59:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from octodir import Octodir
-import os
+import os, sys
+import json
 
 repos = ['es', 'fr', 'zh', 'fa'] # temporal, walk org repos
-Octo = Octodir()
-
 target = 'https://github.com/flaskcwg/flask-docs-{lang}/tree/main/docs/locales/{lang}'
 folder = 'for_deploy/docs' # Current working directory
 
-os.makedirs(folder, exist_ok=True)
-for lang in repos:
-	print("> Downloading: ", lang.upper())
-	Octo.dowload_folder(target.format(lang=lang), folder)
+def download_docs():
+	Octo = Octodir()
+
+	os.makedirs(folder, exist_ok=True)
+	for lang in repos:
+		print("> Downloading: ", lang.upper())
+		Octo.dowload_folder(target.format(lang=lang), folder)
+
+def generate_jsons():
+	pass
+
+def main():
+	if sys.argv[1:]:
+		print(sys.argv[0])
+
+if __name__ == '__main__':
+	main()
