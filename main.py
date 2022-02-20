7ed539a40e2277dbae31b1f263b82a7ebef63215
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2022-02-19 20:30:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,52 @@
 from octodir import Octodir
-import os, sys
+import os
+import sys
 import json
 import requests
 from prettytable import PrettyTable, MARKDOWN
 
+
 repos = [['es', 'es'], ['fr', 'fr'], ['zh', 'zh_CN'], ['fa', 'fa']] # temporal, walk org repos
-target = 'https://github.com/flaskcwg/flask-docs-{lang}/tree/main/docs/locales/{dial}'
-folder = 'for_deploy/docs' # Current working directory
+repo_locales_url = 'https://github.com/flaskcwg/flask-docs-{lang}/tree/main/docs/locales/{dial}'
+docs_dir = 'for_deploy/docs' # Folder for languages
 badge_url = 'https://shields.io/badge/translated-{cov}%25-green'
-img_badge_prev = '![Progress](https://jalkhov.github.io/docspro/badge/{img_url})'
+img_badge_prev = '![Progress](https://jalkhov.github.io/docspro/badge/{img})'
 img_badge_code = f'`{img_badge_prev}`'
+
+
+def round_cov(cov):
+    return "%.2f" % (float(cov))
 
 
 def get_badges():
     return os.listdir("for_deploy/badge")
 
+
 def gen_badges_table():
     table = PrettyTable()
     table.set_style(MARKDOWN)
-    table.field_names = ["Preview", "Code"]
+    table.field_names = ["Lang", "Preview", "Code"]
     badges = get_badges()
 
     for badge in badges:
-        table.add_row([img_badge_prev.format(img_url=badge),
-                       img_badge_code.format(img_url=badge)])
+        lang = bade.split('_')[0]
+        table.add_row([lang,
+                       img_badge_prev.format(img=badge),
+                       img_badge_code.format(img=badge)])
     return table
+
 
 def download_docs():
     Octo = Octodir()
     os.makedirs('for_deploy/data', exist_ok=True) # Create folder for jsons
     os.makedirs('for_deploy/badge', exist_ok=True) # Create folder for badges
-    os.makedirs(folder, exist_ok=True)
+    os.makedirs(docs_dir, exist_ok=True)
     for lang in repos:
         lng = lang[0]
         dlc = lang[1]
 
-        Octo.dowload_folder(target.format(lang=lng, dial=dlc), folder)
+        Octo.dowload_folder(repo_locales_url.format(lang=lng, dial=dlc), docs_dir)
 
-def round_cov(cov):
-    return "%.2f" % (float(cov))
 
 def generate_index():
     table = gen_badges_table()
@@ -53,12 +61,12 @@ def generate_index():
         outfile.write("<h1>Docspro</h1>")
 
 
-
 def generate_jsons(lang, cov):
     cov = round_cov(cov)
     json_string = json.dumps({'cov':cov})
     with open(f'for_deploy/data/{lang}_cov.json', 'w') as outfile:
         outfile.write(json_string)
+
 
 def generate_badge(lang, cov):
     cov = round_cov(cov)
